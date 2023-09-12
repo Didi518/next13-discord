cc0a109e6cc5cc1d9877834df407efd4b1185756
anne torossian <annetorossian@imac-de-anne-torossian.home>
2023-09-12 15:58:04 +0200

    création models prisma :sparkles:

--- app/(main)/(routes)/serveurs/[serverId]/page.tsx
+++ app/(main)/(routes)/serveurs/[serverId]/page.tsx
@@ -1,5 +1,50 @@
-const ServerIdPage = () => {
-  return <div>Serveur ID</div>;
+import { redirectToSignIn } from '@clerk/nextjs';
+import { redirect } from 'next/navigation';
+
+import { currentProfile } from '@/lib/current-profile';
+import { db } from '@/lib/db';
+
+interface ServerIdPageProps {
+  params: {
+    serverId: string;
+  };
+}
+
+const ServerIdPage = async ({ params }: ServerIdPageProps) => {
+  const profile = await currentProfile();
+
+  if (!profile) {
+    return redirectToSignIn();
+  }
+
+  const server = await db.server.findUnique({
+    where: {
+      id: params.serverId,
+      members: {
+        some: {
+          profileId: profile.id,
+        },
+      },
+    },
+    include: {
+      channels: {
+        where: {
+          name: 'général',
+        },
+        orderBy: {
+          createdAt: 'asc',
+        },
+      },
+    },
+  });
+
+  const initialChannel = server?.channels[0];
+
+  if (initialChannel?.name !== 'général') {
+    return null;
+  }
+
+  return redirect(`/serveurs/${params.serverId}/salons/${initialChannel?.id}`);
 };
 
 export default ServerIdPage;
